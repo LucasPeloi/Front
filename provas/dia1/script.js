a6d865adeb10c085d587624e560ed8a17c33589c
Lucas França Peloi <lucaspeloi83@gmail.com>
2026-05-04 00:09:34 -0300

    Add files via upload

---
+++ provas/dia1/script.js
@@ -0,0 +1,23 @@
+function revelar() {
+ 
+    // Altera a imagem principal
+    document.querySelector(".card-img-top").src = "img/_vinicius_junior.png";
+ 
+    // Preenche os spans com as informações
+    document.getElementById("Nome").innerHTML = "Vinícius Júnior <span class='badge text-bg-secondary'>9.5</span>";
+    document.getElementById("Data_Nas").textContent = "12/07/2000 (25 anos)";
+    document.getElementById("Alutra").textContent = "1,76 m";
+    document.getElementById("Posição ").textContent = "Ponta-esquerda / Atacante";
+ 
+    // Remove a classe placeholder e adiciona card-text
+    document.getElementById("Data_Nas").classList.remove("placeholder");
+    document.getElementById("Data_Nas").classList.add("card-text");
+ 
+    document.getElementById("Alutra").classList.remove("placeholder");
+    document.getElementById("Alutra").classList.add("card-text");
+ 
+    document.getElementById("Posição ").classList.remove("placeholder");
+    document.getElementById("Posição ").classList.add("card-text");
+ 
+}
+ 
---
+++ provas/dia2/script.js
@@ -0,0 +1,26 @@
+function add() {
+ 
+    var novoCard = document.createElement("div");
+    novoCard.classList.add("card");
+    novoCard.style.width = "22rem";
+    novoCard.style.margin = "10px";
+ 
+    novoCard.innerHTML = `
+        <img src="img/Lucas_Paqueta.webp" class="card-img-top" alt="Lucas Paquetá">
+        <div class="card-body">
+            <h5 class="card-title">
+                <span class="card-title">Lucas Tolentino Coelho de Lima</span>
+                <span class="badge text-bg-secondary">8,8</span>
+            </h5>
+            <p class="card-text">
+                <span><strong>Nascimento:</strong> 27/08/1997</span><br>
+                <span><strong>Altura:</strong> 1,80 m</span><br>
+                <span><strong>Posição:</strong> Meio-campista</span><br>
+            </p>
+        </div>
+    `;
+ 
+    document.getElementById("Cards").appendChild(novoCard);
+ 
+}
+ 
