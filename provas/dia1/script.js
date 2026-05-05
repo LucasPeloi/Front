b048eb8bb569102cffc252c13ac72f74de5f75bc
Lucas França Peloi <lucaspeloi83@gmail.com>
2026-05-04 21:28:32 -0300

    Update script.js

--- provas/dia1/script.js
+++ provas/dia1/script.js
@@ -1,23 +1,25 @@
 function revelar() {
  
-    // Altera a imagem principal
+    // Troca a imagem placeholder pela foto do Vini Jr
     document.querySelector(".card-img-top").src = "img/_vinicius_junior.png";
  
-    // Preenche os spans com as informações
-    document.getElementById("Nome").innerHTML = "Vinícius Júnior <span class='badge text-bg-secondary'>9.5</span>";
-    document.getElementById("Data_Nas").textContent = "12/07/2000 (25 anos)";
-    document.getElementById("Alutra").textContent = "1,76 m";
-    document.getElementById("Posição ").textContent = "Ponta-esquerda / Atacante";
+    // Preenche o nome no h5 e o rank no span
+    document.getElementById("Nome").innerHTML = "Vinícius Júnior <span class='badge text-bg-secondary'>9,5</span>";
  
-    // Remove a classe placeholder e adiciona card-text
+    // Preenche os spans de informação
+    document.getElementById("Data_Nas").textContent = "Nascimento: 12/07/2000 (25 anos)";
+    document.getElementById("Alutra").textContent = "Altura: 1,76 m";
+    document.getElementById("Posição ").textContent = "Posição: Ponta-esquerda / Atacante";
+ 
+    // Remove placeholder e adiciona card-text
     document.getElementById("Data_Nas").classList.remove("placeholder");
     document.getElementById("Data_Nas").classList.add("card-text");
  
     document.getElementById("Alutra").classList.remove("placeholder");
     document.getElementById("Alutra").classList.add("card-text");
  
-    document.getElementById("Posição").classList.remove("placeholder");
-    document.getElementById("Posição").classList.add("card-text");
+    document.getElementById("Posição ").classList.remove("placeholder");
+    document.getElementById("Posição ").classList.add("card-text");
  
 }
  
